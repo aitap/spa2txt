01653d2b13984a44fb1e474d385b5ebc66bf3946
aitap <krylov.r00t@gmail.com>
2017-10-03 23:57:41 +0300

    possible library interface

---
+++ nicolet_spa.h
@@ -0,0 +1,7 @@
+char ep2_parse(
+	/* in */ const char * /* filename */,
+	/* out, opt */ char * /* comment */,
+	/* out */ size_t * /* num_points */,
+	/* out */ float ** /* wavelengths */,
+	/* out */ float ** /* intensities */
+);

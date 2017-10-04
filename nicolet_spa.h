76dc9f958ef7bf589c8614c3f42bdab02d9f6c23
Ivan Krylov <krylov.r00t@gmail.com>
2017-10-04 15:30:16 +0300

    comment is always 255 characters; include size_t definition

--- nicolet_spa.h
+++ nicolet_spa.h
@@ -1,6 +1,7 @@
+#include <stddef.h>
 char spa_parse(
 	/* in */ const char * /* filename */,
-	/* out, opt */ char * /* comment */,
+	/* out, opt */ char /* comment */ [256],
 	/* out */ size_t * /* num_points */,
 	/* out */ float ** /* wavelengths */,
 	/* out */ float ** /* intensities */
